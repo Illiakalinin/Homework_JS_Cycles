fb0977a56d7651af6e723bb4c9ea74dbc8361c0d
Illia kalinin <illinoiz4305@gmail.com>
2022-11-02 22:22:33 +0200

    Add js cycles

---
+++ js/script.js
@@ -0,0 +1,67 @@
+// const inputValue = Number(prompt("Введіть число:"));
+// if (inputValue == 10) {
+//   console.log("Вірно");
+// } else {
+//   console.log("Невірно");
+// }
+
+// const test = false;
+// if (test) {
+//   console.log("Вірно");
+// } else {
+//   console.log("Невірно");
+// }
+
+// const test = false;
+// if (test != true) {
+//   console.log("Вірно");
+// } else {
+//   console.log("Невірно");
+// }
+
+// const total = Number(prompt("Введіть сумму покупки для розрахування знижки"));
+// if (total >= 500 && total < 800) {
+//   console.log("Знижка = " + total * 0.03 + "грн");
+// } else if (total >= 800) {
+//   console.log("Знижка = " + total * 0.05 + "грн");
+// } else {
+//   console.log("Недостатня сумма для знижки(");
+// }
+
+// let item = 25;
+// while (item >= 0) {
+//   console.log(item);
+//   item--;
+// }
+
+// for (let item = 25; item >= 0; item--) {
+//   console.log(item);
+// }
+
+// let startItem = 10;
+// const finishItem = 50;
+// while (startItem <= finishItem) {
+//   console.log(startItem);
+//   startItem += 5;
+// }
+
+// for (
+//   let startItem = 10, finishItem = 50;
+//   startItem <= finishItem;
+//   startItem += 5
+// ) {
+//   console.log(startItem);
+// }
+
+// let i = 0;
+// let s = 0;
+// while (i++ < 100) {
+//   s += i;
+// }
+// console.log("s = ", s);
+
+let s = 0;
+for (let i = 0; i <= 100; i++) {
+  s += i;
+}
+console.log("s = ", s);
